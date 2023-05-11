aa5aafe519c1803a298a415176573b40a7e2f2e2
zircadz <zircadz@gmail.com>
2023-05-10 18:35:02 -0600

    added translation function and looping through each language set to True in languages.py

---
+++ languages.py
@@ -0,0 +1,521 @@
+afrikaans = 'af'
+irish = 'ga'
+albanian = 'sq'
+italian = 'it'
+arabic = 'ar'
+japanese = 'ja'
+azerbaijani = 'az'
+kannada = 'kn'
+basque = 'eu'
+korean = 'ko'
+bengali = 'bn'
+latin = 'la'
+belarusian = 'be'
+latvian = 'lv'
+bulgarian = 'bg'
+lithuanian = 'lt'
+catalan = 'ca'
+macedonian = 'mk'
+chinese_simplified = 'zh-CN'
+malay = 'ms'
+chinese_traditional = 'zh-TW'
+maltese = 'mt'
+croatian = 'hr'
+norwegian = 'no'
+czech = 'cs'
+persian = 'fa'
+danish = 'da'
+polish = 'pl'
+dutch = 'nl'
+portuguese = 'pt'
+english = 'en'
+romanian = 'ro'
+esperanto = 'eo'
+russian = 'ru'
+estonian = 'et'
+serbian = 'sr'
+filipino = 'tl'
+slovak = 'sk'
+finnish = 'fi'
+slovenian = 'sl'
+french = 'fr'
+spanish = 'es'
+galician = 'gl'
+swahili = 'sw'
+georgian = 'ka'
+swedish = 'sv'
+german = 'de'
+tamil = 'ta'
+greek = 'el'
+telugu = 'te'
+gujarati = 'gu'
+thai = 'th'
+haitian_creole = 'ht'
+turkish = 'tr'
+hebrew = 'iw'
+ukrainian = 'uk'
+hindi = 'hi'
+urdu = 'ur'
+hungarian = 'hu'
+vietnamese = 'vi'
+icelandic = 'is'
+welsh = 'cy'
+indonesian = 'id'
+yiddish = 'yi'
+
+language_list = [
+    'afrikaans',
+    'irish',
+    'albanian',
+    'italian',
+    'arabic',
+    'japanese',
+    'azerbaijani',
+    'kannada',
+    'basque',
+    'korean',
+    'bengali',
+    'latin',
+    'belarusian',
+    'latvian',
+    'bulgarian',
+    'lithuanian',
+    'catalan',
+    'macedonian',
+    'chinese_simplified',
+    'malay',
+    'chinese_traditional',
+    'maltese',
+    'croatian',
+    'norwegian',
+    'czech',
+    'persian',
+    'danish',
+    'polish',
+    'dutch',
+    'portuguese',
+    'english',
+    'romanian',
+    'esperanto',
+    'russian',
+    'estonian',
+    'serbian',
+    'filipino',
+    'slovak',
+    'finnish',
+    'slovenian',
+    'french',
+    'spanish',
+    'galician',
+    'swahili',
+    'georgian',
+    'swedish',
+    'german',
+    'tamil',
+    'greek',
+    'telugu',
+    'gujarati',
+    'thai',
+    'haitian_creole',
+    'turkish',
+    'hebrew',
+    'ukrainian',
+    'hindi',
+    'urdu',
+    'hungarian',
+    'vietnamese',
+    'icelandic',
+    'welsh',
+    'indonesian',
+    'yiddish'
+]
+language_list_alphabetical_order = [
+    'afrikaans',
+    'albanian',
+    'arabic',
+    'azerbaijani',
+    'basque',
+    'belarusian',
+    'bengali',
+    'bulgarian',
+    'catalan',
+    'chinese_simplified',
+    'chinese_traditional',
+    'croatian',
+    'czech',
+    'danish',
+    'dutch',
+    'english',
+    'esperanto',
+    'estonian',
+    'filipino',
+    'finnish',
+    'french',
+    'galician',
+    'georgian',
+    'german',
+    'greek',
+    'gujarati',
+    'haitian_creole',
+    'hebrew',
+    'hindi',
+    'hungarian',
+    'icelandic',
+    'indonesian',
+    'irish',
+    'italian',
+    'japanese',
+    'kannada',
+    'korean',
+    'latin',
+    'latvian',
+    'lithuanian',
+    'macedonian',
+    'malay',
+    'maltese',
+    'norwegian',
+    'persian',
+    'polish',
+    'portuguese',
+    'romanian',
+    'russian',
+    'serbian',
+    'slovak',
+    'slovenian',
+    'spanish',
+    'swahili',
+    'swedish',
+    'tamil',
+    'telugu',
+    'thai',
+    'turkish',
+    'ukrainian',
+    'urdu',
+    'vietnamese',
+    'welsh',
+    'yiddish',
+]
+
+language_list_dict = [
+    {
+        'language': 'afrikaans',
+        'status': False,
+        'code': 'af'
+    },
+    {
+        'language': 'albanian',
+        'status': False,
+        'code': 'sq'
+    },
+    {
+        'language': 'arabic',
+        'status': False,
+        'code': 'ar'
+    },
+    {
+        'language': 'azerbaijani',
+        'status': False,
+        'code': 'az'
+    },
+    {
+        'language': 'basque',
+        'status': False,
+        'code': 'eu'
+    },
+    {
+        'language': 'belarusian',
+        'status': False,
+        'code': 'be'
+    },
+    {
+        'language': 'bengali',
+        'status': False,
+        'code': 'bn'
+    },
+    {
+        'language': 'bulgarian',
+        'status': False,
+        'code': 'bg'
+    },
+    {
+        'language': 'catalan',
+        'status': False,
+        'code': 'ca'
+    },
+    {
+        'language': 'chinese_simplified',
+        'status': False,
+        'code': 'zh-CN'
+    },
+    {
+        'language': 'chinese_traditional',
+        'status': False,
+        'code': 'zh-TW'
+    },
+    {
+        'language': 'croatian',
+        'status': False,
+        'code': 'hr'
+    },
+    {
+        'language': 'czech',
+        'status': False,
+        'code': 'cs'
+    },
+    {
+        'language': 'danish',
+        'status': True,
+        'code': 'da'
+    },
+    {
+        'language': 'dutch',
+        'status': False,
+        'code': 'nl'
+    },
+    {
+        'language': 'english',
+        'status': False,
+        'code': 'en'
+    },
+    {
+        'language': 'esperanto',
+        'status': False,
+        'code': 'eo'
+    },
+    {
+        'language': 'estonian',
+        'status': True,
+        'code': 'et'
+    },
+    {
+        'language': 'filipino',
+        'status': False,
+        'code': 'tl'
+    },
+    {
+        'language': 'finnish',
+        'status': True,
+        'code': 'fi'
+    },
+    {
+        'language': 'french',
+        'status': False,
+        'code': 'fr'
+    },
+    {
+        'language': 'galician',
+        'status': False,
+        'code': 'gl'
+    },
+    {
+        'language': 'georgian',
+        'status': False,
+        'code': 'ka'
+    },
+    {
+        'language': 'german',
+        'status': False,
+        'code': 'de'
+    },
+    {
+        'language': 'greek',
+        'status': False,
+        'code': 'el'
+    },
+    {
+        'language': 'gujarati',
+        'status': False,
+        'code': 'gu'
+    },
+    {
+        'language': 'haitian_creole',
+        'status': False,
+        'code': 'ht'
+    },
+    {
+        'language': 'hebrew',
+        'status': False,
+        'code': 'iw'
+    },
+    {
+        'language': 'hindi',
+        'status': False,
+        'code': 'hi'
+    },
+    {
+        'language': 'hungarian',
+        'status': False,
+        'code': 'hu'
+    },
+    {
+        'language': 'icelandic',
+        'status': False,
+        'code': 'is'
+    },
+    {
+        'language': 'indonesian',
+        'status': False,
+        'code': 'id'
+    },
+    {
+        'language': 'irish',
+        'status': False,
+        'code': 'ga'
+    },
+    {
+        'language': 'italian',
+        'status': False,
+        'code': 'it'
+    },
+    {
+        'language': 'japanese',
+        'status': False,
+        'code': 'ja'
+    },
+    {
+        'language': 'kannada',
+        'status': False,
+        'code': 'kn'
+    },
+    {
+        'language': 'korean',
+        'status': False,
+        'code': 'ko'
+    },
+    {
+        'language': 'latin',
+        'status': False,
+        'code': 'la'
+    },
+    {
+        'language': 'latvian',
+        'status': False,
+        'code': 'lv'
+    },
+    {
+        'language': 'lithuanian',
+        'status': False,
+        'code': 'lt'
+    },
+    {
+        'language': 'macedonian',
+        'status': False,
+        'code': 'mk'
+    },
+    {
+        'language': 'malay',
+        'status': False,
+        'code': 'ms'
+    },
+    {
+        'language': 'maltese',
+        'status': False,
+        'code': 'mt'
+    },
+    {
+        'language': 'norwegian',
+        'status': False,
+        'code': 'no'
+    },
+    {
+        'language': 'persian',
+        'status': False,
+        'code': 'fa'
+    },
+    {
+        'language': 'polish',
+        'status': True,
+        'code': 'pl'
+    },
+    {
+        'language': 'portuguese',
+        'status': False,
+        'code': 'pt'
+    },
+    {
+        'language': 'romanian',
+        'status': False,
+        'code': 'ro'
+    },
+    {
+        'language': 'russian',
+        'status': False,
+        'code': 'ru'
+    },
+    {
+        'language': 'serbian',
+        'status': False,
+        'code': 'sr'
+    },
+    {
+        'language': 'slovak',
+        'status': False,
+        'code': 'sk'
+    },
+    {
+        'language': 'slovenian',
+        'status': False,
+        'code': 'sl'
+    },
+    {
+        'language': 'spanish',
+        'status': False,
+        'code': 'es'
+    },
+    {
+        'language': 'swahili',
+        'status': False,
+        'code': 'sw'
+    },
+    {
+        'language': 'swedish',
+        'status': False,
+        'code': 'sv'
+    },
+    {
+        'language': 'tamil',
+        'status': False,
+        'code': 'ta'
+    },
+    {
+        'language': 'telugu',
+        'status': False,
+        'code': 'te'
+    },
+    {
+        'language': 'thai',
+        'status': False,
+        'code': 'th'
+    },
+    {
+        'language': 'turkish',
+        'status': False,
+        'code': 'tr'
+    },
+    {
+        'language': 'ukrainian',
+        'status': False,
+        'code': 'uk'
+    },
+    {
+        'language': 'urdu',
+        'status': False,
+        'code': 'ur'
+    },
+    {
+        'language': 'vietnamese',
+        'status': False,
+        'code': 'vi'
+    },
+    {
+        'language': 'welsh',
+        'status': False,
+        'code': 'cy'
+    },
+    {
+        'language': 'yiddish',
+        'status': False,
+        'code': 'yi'
+    }
+
+]
--- main.py
+++ main.py
@@ -1,29 +1,47 @@
 # Multi-Language Translation Tool
 import os
 from google.cloud import translate
-from dotenv import load_dotenv 
-
+from dotenv import load_dotenv
+from languages import *
 load_dotenv()
 
 PROJECT_ID = str(os.getenv('PROJECT_ID'))
+newlist = sorted(language_list)
+newlanguages = []
+codes = []
+for c in language_list_dict:
+    if c["status"] == True:
+        language_to_translate = c["language"]
+        language_code = c["code"]
+        newlanguages.append(language_to_translate)
+        codes.append(language_code)
 
-def translate_text(
-        text="Hello, world!", project_id=PROJECT_ID):
+input_string = input("Enter a string to translate: ")
+print("Word to be translated: ", input_string)
+output_language = newlanguages
+print(output_language)
 
+
+# HERE IS WHERE WE NEED TO RUN THE LANGUAGE CODE THROUGH THE NEXT FUNCTION
+
+def translate_text(text=input_string, project_id=PROJECT_ID):
+    OUTPUT_LANGUAGE = i
     client = translate.TranslationServiceClient()
     location = "global"
     parent = f"projects/{project_id}/locations/{location}"
-
     response = client.translate_text(
         request={
             "parent": parent,
             "contents": [text],
             "mime_type": "text/plain",
-            "source_language_code": "en-US",
-            "target_language_code": "es",
+            "source_language_code": 'en-US',
+            "target_language_code": OUTPUT_LANGUAGE,
         }
     )
-
     for translation in response.translations:
         print("Translated text: {}".format(translation.translated_text))
-translate_text()
+
+
+for i in output_language and codes:
+    translate_text()
+    print(i)

f30acc0d73334c7805b02c5d9d18e9f1092c32a6
zircadz <zircadz@gmail.com>
2023-05-12 09:34:01 -0600

    added django backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--85c9j9n&b7a(ompx%ie8hxdm3rs#ltv$4*r8=(5@i1jtg@$nt'
+SECRET_KEY = 'BACKEND_SECRET'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
